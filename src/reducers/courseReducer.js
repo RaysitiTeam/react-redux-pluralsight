4c8bd206dd99feff3f372211026b4fb02d951728
Pramod Jingade <Pramod Jingade>
2017-05-04 23:43:39 +0530

    Fixed all eslint issues

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -3,8 +3,7 @@ export default function courseReducer(state=[],action){
     switch(action.type){
         case 'CREATE_COURSE':
             // state.push(action.course); //State is immutable, I shouldn't be mutating the state.
-            return [...state,Object.assign({},action.course)]; // This way we are passing a copy of state, not the mutated state.
-            break;
+            return [...state,Object.assign({},action.course)]; // This way we are passing a copy of state, not the mutated state.            
         default: 
             return state; //Any other action, we can just return the current state.
     }//end:switch
